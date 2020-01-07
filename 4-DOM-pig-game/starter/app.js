33e87e848365612897dc4709e6e42db90cad6378
mahejon <jmaher1@gmail.com>
2020-01-07 16:09:57 -0500

    first commit

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -8,3 +8,95 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, activePlayer, roundScore, gamePlaying, rolls;
+
+init()
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    
+    if (gamePlaying) {
+        var dice = Math.floor(Math.random() * 6) + 1;
+        rolls.shift()
+        rolls.push(dice);
+        console.log(rolls);
+        
+        if ((rolls[0] + rolls[1]) === 12) {
+            scores[activePlayer] = 0;
+            document.querySelector('#score-' + activePlayer).textContent = 0;
+            resetRolls();
+            nextPlayer();
+
+        } else if (dice !== 1) {
+            roundScore  += dice;
+            document.querySelector('.dice-1').style.display = 'block';
+            document.querySelector('.dice-1').src = 'dice-' + dice + '.png';
+            document.querySelector('#current-' + activePlayer).textContent = roundScore;
+        } else {
+            nextPlayer();
+        }
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function(){
+    
+    if (gamePlaying) {
+        scores[activePlayer] += roundScore;
+        document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+        resetRolls();
+        
+        if (scores[activePlayer] >= 100) {
+            document.querySelector('#name-' + activePlayer).textContent = 'WINNER!';
+            document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+            gamePlaying = false;
+        } else {
+            nextPlayer();
+        };
+    };
+});
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+
+function init() {
+    scores = [0,0];
+    activePlayer = 0;
+    roundScore = 0;
+    gamePlaying = true;
+    resetRolls()
+    
+    document.querySelector('#score-0').textContent = 0;
+    document.querySelector('#score-1').textContent = 0;
+    document.querySelector('#current-0').textContent = 0;
+    document.querySelector('#current-1').textContent = 0;
+    document.querySelector('.player-0-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('winner');
+    document.querySelector('.player-1-panel').classList.remove('active');
+    document.querySelector('.player-0-panel').classList.add('active');
+    document.querySelector('.dice-1').style.display = 'none';
+    
+}
+
+
+function nextPlayer() {
+    document.getElementById('current-' + activePlayer).textContent = 0;
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+    document.querySelector('.dice-1').style.display = 'none';
+    roundScore = 0;
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+
+}
+
+function resetRolls() {
+    rolls = [0, 0];
+}
+
+
+/* YOUR 3 CHALLENGES
+Change the game to follow these rules:
+
+1. A player looses his ENTIRE score when he rolls two 6 in a row. After that, it's the next player's turn. (Hint: Always save the previous dice roll in a separate variable)
+2. Add an input field to the HTML where players can set the winning score, so that they can change the predefined score of 100. (Hint: you can read that value with the .value property in JavaScript. This is a good oportunity to use google to figure this out :)
+3. Add another dice to the game, so that there are two dices now. The player looses his current score when one of them is a 1. (Hint: you will need CSS to position the second dice, so take a look at the CSS code for the first one.)
+ */
